4d5fb40b4f2afb7b68e7149643f67b2cd8c44ce5
Jay <jaysondhi1@gmail.com>
2016-02-27 16:04:49 +0000

    Working Navigation system

--- app/src/main/java/info/androidhive/loginandregistration/activity/MainActivity.java
+++ app/src/main/java/info/androidhive/loginandregistration/activity/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -15,63 +16,128 @@ import info.androidhive.loginandregistration.helper.SessionManager;
 
 public class MainActivity extends Activity {
 
-	private TextView txtName;
-	private TextView txtEmail;
-	private Button btnLogout;
+    private TextView txtName;
+    private TextView txtEmail;
+    private ImageButton btnLogout;
+    private ImageButton imageButton;
+    private ImageButton imageButton2;
+    private ImageButton imageButton3;
+    private ImageButton imageButton4;
+    private ImageButton imageButton5;
+    private ImageButton imageButton6;
 
-	private SQLiteHandler db;
-	private SessionManager session;
+    private SQLiteHandler db;
+    private SessionManager session;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-		txtName = (TextView) findViewById(R.id.name);
-		txtEmail = (TextView) findViewById(R.id.email);
-		btnLogout = (Button) findViewById(R.id.btnLogout);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		// SqLite database handler
-		db = new SQLiteHandler(getApplicationContext());
+        txtName = (TextView) findViewById(R.id.name);
+        txtEmail = (TextView) findViewById(R.id.email);
+        btnLogout = (ImageButton) findViewById(R.id.btnLogout);
+        ImageButton btn = (ImageButton)findViewById(R.id.imageButton);
+        ImageButton btn2 = (ImageButton)findViewById(R.id.imageButton2);
+        ImageButton btn3 = (ImageButton)findViewById(R.id.imageButton3);
+        ImageButton btn4 = (ImageButton)findViewById(R.id.imageButton4);
+        ImageButton btn5 = (ImageButton)findViewById(R.id.imageButton5);
+        ImageButton btn6 = (ImageButton)findViewById(R.id.imageButton6);
 
-		// session manager
-		session = new SessionManager(getApplicationContext());
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MarketInfo.class);
+                startActivity(intent);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ResearchInfo.class);
+                startActivity(intent);
+            }
+        });
 
-		if (!session.isLoggedIn()) {
-			logoutUser();
-		}
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Calendar.class);
+                startActivity(intent);
+            }
+        });
 
-		// Fetching user details from SQLite
-		HashMap<String, String> user = db.getUserDetails();
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContactList.class);
+                startActivity(intent);
+            }
+        });
 
-		String name = user.get("name");
-		String email = user.get("email");
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanContact.class);
+                startActivity(intent);
+            }
+        });
 
-		// Displaying the user details on the screen
-		txtName.setText(name);
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Reminders.class);
+                startActivity(intent);
+            }
+        });
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
 
-		// Logout button click event
-		btnLogout.setOnClickListener(new View.OnClickListener() {
+        // session manager
+        session = new SessionManager(getApplicationContext());
 
-			@Override
-			public void onClick(View v) {
-				logoutUser();
-			}
-		});
-	}
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
 
-	/**
-	 * Logging out the user. Will set isLoggedIn flag to false in shared
-	 * preferences Clears the user data from sqlite users table
-	 * */
-	private void logoutUser() {
-		session.setLogin(false);
+        // Fetching user details from SQLite
+        HashMap<String, String> user = db.getUserDetails();
 
-		db.deleteUsers();
+        String name = user.get("name");
+        String email = user.get("email");
 
-		// Launching the login activity
-		Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-		startActivity(intent);
-		finish();
-	}
-}
+        // Displaying the user details on the screen
+        txtName.setText(name);
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+    }
+
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     */
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+
+
+
+
+    }
---
+++ app/src/main/java/info/androidhive/loginandregistration/activity/MarketInfo.java
@@ -0,0 +1,16 @@
+package info.androidhive.loginandregistration.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import info.androidhive.loginandregistration.R;
+
+public class MarketInfo extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market_info);
+    }
+
+}
